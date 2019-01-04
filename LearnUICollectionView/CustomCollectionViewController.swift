13d832d46c18855cf7beddd5438306e509d3b858
admin <admin.@admins-MacBook-Pro.local>
2019-01-03 18:41:08 -0600

    First commit

---
+++ LearnUICollectionView/CustomCollectionViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  LearnUICollectionView
+//
+//  Created by  admin on 1/3/19.
+//  Copyright © 2019  admin. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    let customCellIdentifier = "customCellIdentifier"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        collectionView?.backgroundColor = UIColor.white
+        
+        collectionView?.register(CustomCell.self, forCellWithReuseIdentifier: customCellIdentifier)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let customCell = collectionView.dequeueReusableCell(withReuseIdentifier: customCellIdentifier, for: indexPath) as! CustomCell
+        customCell.nameLabel.text = strings[indexPath.item]
+        return customCell
+    }
+    
+    let strings = ["Chirag Gandhi", "is LEARNING", "UIVIEWCOLLECTION w/o STORYBOARD!"]
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let sizing = CGSize(width: view.frame.width, height: 200)
+        return sizing
+    }
+    
+}
+
+class CustomCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Custom Text"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    func setupViews() {
+        backgroundColor = UIColor.red
+        
+        addSubview(nameLabel)
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
